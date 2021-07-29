3fa0df4fbb4c5515630ca5d59dc252587deae92f
Keane <KeaneCarotenuto@gmail.com>
2021-07-29 14:27:33 +1200

    Enemies attack + Player Health + Visual Health

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float m_health = 100;
+
+    public bool dead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float _dmg)
+    {
+        if (!dead)
+        {
+            m_health -= _dmg;
+
+            UpdateAppearance();
+
+            if (m_health <= 0)
+            {
+                dead = true;
+                m_health = 0;
+            }
+        }
+    }
+
+    public void UpdateAppearance()
+    {
+        GetComponent<ParticleSystemRenderer>().material.color = new Color(m_health / 100.0f, 0, 0);
+        GetComponent<MeshRenderer>().material.color = new Color(m_health / 100.0f, 0, 0);
+
+        ParticleSystem.VelocityOverLifetimeModule vel = GetComponent<ParticleSystem>().velocityOverLifetime;
+        vel.speedModifierMultiplier = m_health / 100.0f;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        TakeDamage(0.05f);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,30 +6,36 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float moveSpeed;
+    public float m_moveSpeed;
 
-    public Vector3 velocity;
+    public Vector3 m_velocity;
+
+    PlayerHealth m_healthComp;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_healthComp = GetComponent<PlayerHealth>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (m_healthComp.dead)
+        {
+            return;
+        }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = Vector3.ClampMagnitude(new Vector3(x + z,0,z - x), 1.0f);
+        m_velocity = Vector3.ClampMagnitude(new Vector3(x + z,0,z - x), 1.0f) * m_moveSpeed;
 
-        controller.Move(move * Time.deltaTime * moveSpeed);
+        controller.Move(m_velocity * Time.deltaTime);
 
-        if (move.magnitude > 0)
+        if (m_velocity.magnitude > 0)
         {
-            transform.LookAt(transform.position + move);
+            transform.LookAt(transform.position + m_velocity);
         }
         
     }
