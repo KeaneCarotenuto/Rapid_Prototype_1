23fe7a791f6056eabb76d90765ff4e0250d91e62
Keane <KeaneCarotenuto@gmail.com>
2021-07-30 22:45:44 +1200

    Spreading Fire + Better health visual

---
+++ Assets/Scripts/Flamable.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamable : MonoBehaviour
+{
+    public bool onFire = false;
+
+    public GameObject fire;
+    public GameObject light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.layer = 11;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (onFire && other.layer == 12)
+        {
+            gameObject.layer = 11;
+
+            ParticleSystem ps = GetComponent<ParticleSystem>();
+            if (ps)
+            {
+                if (GetComponentInChildren<Light>())
+                {
+                    GetComponentInChildren<Light>().enabled = false;
+                }
+
+                ps.Stop();
+                //Destroy(ps, 5);
+                onFire = false;
+            }
+        }
+        else if ((!GetComponent<ParticleSystem>() || (GetComponent<ParticleSystem>() && GetComponent<ParticleSystem>().isStopped)) && gameObject.layer == 11 && other.layer != 12)
+        {
+            onFire = true;
+            gameObject.layer = 10;
+
+            if (GetComponentInChildren<Light>()) GetComponentInChildren<Light>().enabled = true;
+            else Instantiate(light, transform, false);
+
+            ParticleSystem ps = GetComponent<ParticleSystem>();
+
+            if (!ps)
+            {
+                GetComponent<MeshRenderer>().material.color *= 0.1f; 
+
+                ps = gameObject.AddComponent<ParticleSystem>();
+
+                UnityEditorInternal.ComponentUtility.CopyComponent(fire.GetComponent<ParticleSystem>());
+                UnityEditorInternal.ComponentUtility.PasteComponentValues(ps);
+
+                ParticleSystem.ShapeModule sm = ps.shape;
+                sm.shapeType = ParticleSystemShapeType.MeshRenderer;
+                sm.meshShapeType = ParticleSystemMeshShapeType.Triangle;
+                sm.meshRenderer = GetComponent<MeshRenderer>();
+                
+            }
+
+            ps.Play(); 
+        }
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -9,10 +9,16 @@ public class PlayerHealth : MonoBehaviour
 
     public bool dead = false;
 
+    Color copyCol;
+    Color copyParticleCol;
+    Color copyParticleEmmisionCol;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        copyCol = GetComponent<MeshRenderer>().material.color;
+        copyParticleCol = GetComponent<ParticleSystemRenderer>().material.color;
+        copyParticleEmmisionCol = GetComponent<ParticleSystemRenderer>().material.GetColor("_EmissionColor");
     }
 
     // Update is called once per frame
@@ -49,8 +55,11 @@ public class PlayerHealth : MonoBehaviour
 
     public void UpdateAppearance()
     {
-        GetComponent<ParticleSystemRenderer>().material.color = new Color(m_health / 100.0f, 0, 0);
-        GetComponent<MeshRenderer>().material.color = new Color(m_health / 100.0f, 0, 0);
+        //Fix the alpha being changed on colour change
+
+        GetComponent<ParticleSystemRenderer>().material.color = copyParticleCol * (m_health / 100.0f);
+        GetComponent<ParticleSystemRenderer>().material.SetColor("_EmissionColor", copyParticleEmmisionCol * (m_health / 100.0f));
+        GetComponent<MeshRenderer>().material.color = copyCol * (m_health / 100.0f);
 
         ParticleSystem.VelocityOverLifetimeModule vel = GetComponent<ParticleSystem>().velocityOverLifetime;
         vel.speedModifierMultiplier = m_health / 100.0f;
