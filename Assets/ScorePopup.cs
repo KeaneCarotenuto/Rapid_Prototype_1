605d237fd09953dbdb558dd37460367ea6e1d761
Nerys-Thamm <nerysthamm@gmail.com>
2021-07-31 22:05:39 +1200

    tried adding some text to pop up when you earn score, for some reason my IDE isnt letting me include the UI namespace so its not working

---
+++ Assets/ScorePopup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScorePopup : MonoBehaviour
+{
+    public int m_Score;
+
+    public Text m_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
