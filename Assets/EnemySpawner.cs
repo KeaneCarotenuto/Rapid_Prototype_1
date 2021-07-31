a16038994b66f2cf8cec2f4d9f64f1a8c66f53f8
Nerys <NerysThamm@gmail.com>
2021-07-31 12:00:42 +1200

    Made the prop system able to spawn enemies and slightly improved its functionality

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PropSpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
 
     public PropList m_PropList;
+    public GameObject m_EnemyPrefab;
     public float m_RoomSize;
     public int m_TileNum;
 
@@ -25,8 +26,8 @@ public class PropSpawner : MonoBehaviour
             for (var j = 0; j < m_TileNum; j++)
             {
                 Vector3 tilepos = this.transform.position - new Vector3((m_RoomSize / 2) - tilesize / 2, 0, (m_RoomSize / 2) - tilesize / 2) + new Vector3(i * tilesize, 0, j * tilesize);
-                bool isfloor = Physics.CheckBox(tilepos - new Vector3(0, 2, 0), new Vector3(tilesize / 2, 1 / 2, tilesize / 2), Quaternion.identity, LayerMask.GetMask("Ground"));
-                bool isoccupied = Physics.CheckBox(tilepos, new Vector3(tilesize / 2, 1 / 2, tilesize / 2), Quaternion.identity, LayerMask.GetMask("Ground"));
+                bool isfloor = Physics.CheckBox(tilepos - new Vector3(0, 2, 0), new Vector3(tilesize / 2, 0.5f / 2, tilesize / 2), Quaternion.identity, LayerMask.GetMask("Ground") | LayerMask.GetMask("Environment") | LayerMask.GetMask("Flamable"));
+                bool isoccupied = Physics.CheckBox(tilepos, new Vector3(tilesize / 4, 1.0f / 2, tilesize / 4), Quaternion.identity, LayerMask.GetMask("Ground") | LayerMask.GetMask("Environment") | LayerMask.GetMask("Flamable"));
                 m_ValidTiles[i, j] = (isfloor) && (!isoccupied);
 
 
@@ -51,6 +52,10 @@ public class PropSpawner : MonoBehaviour
                         GameObject.Instantiate(m_PropList.Prefabs[Random.Range(0, m_PropList.Prefabs.Count)], tilepos - new Vector3(0, 1, 0), Quaternion.identity);
 
                     }
+                    else if (Random.Range(0, 4) == 0)
+                    {
+                        GameObject.Instantiate(m_EnemyPrefab, tilepos - new Vector3(0, 0, 0), Quaternion.identity);
+                    }
                 }
 
 
@@ -62,7 +67,28 @@ public class PropSpawner : MonoBehaviour
 
     public void GenerateEnemies()
     {
+        CheckTiles();
+        float tilesize = m_RoomSize / m_TileNum;
+        for (var i = 0; i < m_TileNum; i++)
+        {
+            for (var j = 0; j < m_TileNum; j++)
+            {
+                if (m_ValidTiles[i, j])
+                {
+                    Vector3 tilepos = this.transform.position - new Vector3((m_RoomSize / 2) - tilesize / 2, 0, (m_RoomSize / 2) - tilesize / 2) + new Vector3(i * tilesize, 0, j * tilesize);
 
+                    if (Random.Range(0, 3) == 0)
+                    {
+                        
+
+                    }
+                }
+
+
+
+
+            }
+        }
     }
 
     // Start is called before the first frame update
