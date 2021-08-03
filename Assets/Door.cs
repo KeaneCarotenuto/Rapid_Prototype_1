94a94ae437c87bada1ce1a25c4b4a2e070c4d250
Nerys-Thamm <nerysthamm@gmail.com>
2021-08-03 22:04:55 +1200

    DOOR PLS

---
+++ Assets/Door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool m_IsOpen = true;
+
+    public Animator m_Anim;
+
+    public GameObject m_ColliderObj;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Anim.SetBool("Open", m_IsOpen);
+        m_ColliderObj.SetActive(!m_IsOpen);
+    }
+}
